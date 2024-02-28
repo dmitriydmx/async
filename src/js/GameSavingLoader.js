6ae7a226b5907ba0e567556502c40ca64c775a0a
dmitriydmx <dmitriydmx@mail.ru>
2024-02-28 18:34:10 +0900

    fixed async

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,14 +1,19 @@
-import read from './reader.js';
 import json from './parser.js';
+import read from './reader.js';
+import GameSaving from './GameSaving.js';
 
 export default class GameSavingLoader {
-  static async load() {
-    try {
-      const data = await read();
-      const value = await json(data);
-      return JSON.parse(value);
-    } catch (error) {
-      throw new Error(error);
-    }
+  static load() {
+    return new Promise((resolve, reject) => {
+      read()
+        .then((data) => json(data))
+        .then((parsedData) => {
+          const saving = new GameSaving(JSON.parse(parsedData));
+          resolve(saving);
+        })
+        .catch((error) => {
+          reject(error);
+        });
+    });
   }
 }
